e46f9c91a6f2a1c2155dafdcacc14d158d087300
Carl.Chen <iamcarlchen@gmail.com>
2026-05-27 14:00:36 +0800

    update isse

--- nitro.config.ts
+++ nitro.config.ts
@@ -4,4 +4,7 @@ export default defineNitroConfig({
   srcDir: 'server',
   compatibilityDate: '2026-04-30',
   preset: process.env.NITRO_PRESET || 'vercel',
+  externals: {
+    inline: ['mokelay-server-core'],
+  },
 })
